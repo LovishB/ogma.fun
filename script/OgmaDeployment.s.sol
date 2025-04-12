65649adb04bc98c89b100383478924deeb32c028
Lovish Badlani <lovish@dxsale.network>
2025-04-12 15:45:25 +0530

    feat: Initial Ogma Foundry Setup

---
+++ script/OgmaDeployment.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {OgmaDeployer} from "../src/OgmaDeployer.sol";
+import {OgmaToken} from "../src/OgmaToken.sol";
+
+contract OgmaDeploymentScript {
+    
+}
