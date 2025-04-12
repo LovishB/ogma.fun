af9e561e263a403c0c776d00c8779ebb63349f14
Lovish Badlani <lovish@dxsale.network>
2025-04-12 21:19:35 +0530

    feat: Deployment Script

--- script/OgmaDeployment.s.sol
+++ script/OgmaDeployment.s.sol
@@ -3,7 +3,27 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {OgmaToken} from "../src/OgmaToken.sol";
+import {OgmaStorage} from "../src/OgmaStorage.sol";
+import {OgmaTokenLock} from "../src/OgmaTokenLock.sol";
+import {OgmaFactory} from "../src/OgmaFactory.sol";
 
-contract OgmaDeploymentScript {
-    
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract OgmaDeployment is Script {
+
+    function run() external {
+         HelperConfig helperConfig = new HelperConfig();
+        (uint256 deployerKey, ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast(deployerKey);
+        OgmaStorage ogmaStorage = new OgmaStorage();
+        OgmaTokenLock ogmaTokenLock = new OgmaTokenLock(address(ogmaStorage));
+        OgmaFactory ogmaFactory = new OgmaFactory(address(ogmaStorage), address(ogmaTokenLock));
+
+        console.log("ogmaStorage", address(ogmaStorage));
+        console.log("ogmaTokenLock", address(ogmaTokenLock));
+        console.log("ogmaFactory", address(ogmaFactory));
+
+        vm.stopBroadcast();
+    }
 }
