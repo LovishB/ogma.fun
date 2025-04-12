b86ffda525e3339eeaa9f7c294e33ac80f02026a
Lovish Badlani <lovish@dxsale.network>
2025-04-12 16:11:57 +0530

    feat: Implemented Ogma Token Contract

--- src/OgmaToken.sol
+++ src/OgmaToken.sol
@@ -1,6 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract OgmaToken {
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+An ERC20 token implementation for Ogma Launchpad.
+Tokens are minted with max supply (1 million tokens, 18 decimals) at deployment.
+*/
+contract OgmaToken is ERC20, Ownable {
+
+    string public s_tokenURI;
+    string public s_tokenDescription;
+    uint256 public constant MAX_SUPPLY = 1000000 * 10 ** 18;
+
+    /**
+    * @dev Constructor that mints the entire supply to the owner
+    * @param _tokenName Name of the token
+    * @param _tokenSymbol Symbol of the token
+    * @param _tokenURI URI for token metadata/image
+    * @param _tokenDescription Description of the token
+    * @param _tokenOwner Address of the token owner
+    */
+    constructor(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        string memory _tokenURI,
+        string memory _tokenDescription,
+        address _tokenOwner
+    ) ERC20(_tokenName, _tokenSymbol) Ownable(_tokenOwner){
+        // Minting entire supply
+        _mint(_tokenOwner, MAX_SUPPLY);
+        s_tokenURI = _tokenURI;
+        s_tokenDescription = _tokenDescription;
+    }
 
 }
