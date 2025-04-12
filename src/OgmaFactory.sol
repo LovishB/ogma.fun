8bc5952b1d9259c01c20fbda5f79e22cbd538af7
Lovish Badlani <lovish@dxsale.network>
2025-04-12 17:06:45 +0530

    feat: Ogma Factory and Storage Contract

--- src/OgmaFactory.sol
+++ src/OgmaFactory.sol
@@ -1,6 +1,74 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract OgmaFactory {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {OgmaToken} from "./OgmaToken.sol";
+import {OgmaStorage} from "./OgmaStorage.sol";
+
+/**
+ * @title OgmaFactory
+ * @dev Factory contract for creating OgmaToken instances.
+ * 1) Factory mints the entire supply to the owner
+ * 2) Lock 60% of the supply for 3 months
+ */
+contract OgmaFactory is Ownable {
+    OgmaStorage public ogmaStorage;
+
+    event TokenMinted(
+        address indexed tokenAddress,
+        string tokenName,
+        string tokenSymbol,
+        string tokenURI,
+        string tokenDescription
+    );
+
+    constructor(address _ogmaStorage) Ownable(msg.sender) {
+        ogmaStorage = OgmaStorage(_ogmaStorage);
+    }
+    
+    /**
+     * @dev Creates a new OgmaToken instance and mints the entire supply to the owner.
+     * @param _tokenName Name of the token
+     * @param _tokenSymbol Symbol of the token
+     * @param _tokenURI URI for token metadata/image
+     * @param _tokenDescription Description of the token
+     * @return The address of the newly created OgmaToken instance
+     */
+    function createToken (
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        string memory _tokenURI,
+        string memory _tokenDescription
+    ) external returns (address) {
+        OgmaToken newToken = new OgmaToken(
+            _tokenName,
+            _tokenSymbol,
+            _tokenURI,
+            _tokenDescription,
+            msg.sender
+        );
+
+        // Lock 60% of the supply for 3 months
+
+        ogmaStorage.registerToken(
+            address(newToken),
+            _tokenName,
+            _tokenSymbol,
+            _tokenURI,
+            _tokenDescription,
+            // newToken.totalSupply(),
+            // newToken.lockedSupply(),
+            // newToken.unlockDate()
+        );
+
+        emit TokenMinted(
+            address(newToken),
+            _tokenName,
+            _tokenSymbol,
+            _tokenURI,
+            _tokenDescription
+        );
+        return address(newToken);
+    }
 
 }
--- src/OgmaStorage.sol
+++ src/OgmaStorage.sol
@@ -1,6 +1,115 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract OgmaStorage {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title OgmaStorage
+ * @dev Storage contract for tracking all tokens created through OgmaFactory
+ */
+contract OgmaStorage is Ownable {
+
+    struct TokenInfo {
+        string name;
+        string symbol;
+        string uri;
+        string description;
+        address owner;
+        uint256 totalSupply;
+        uint256 lockedSupply;
+        uint256 unlockDate;
+    }
+    address[] public s_tokenAddresses;
+    mapping(address => TokenInfo) public s_tokens;
+    address private s_ogmaFactory;
+    
+    constructor() Ownable(msg.sender) {}
+
+    modifier isFactoryAutorized() {
+        if (msg.sender != s_ogmaFactory) {
+            revert("Not authorized to register token");
+        }
+        _;
+    }
+
+    /**
+     * @dev Register a new token in storage
+     * @param _tokenAddress Address of the token contract
+     * @param _name Name of the token
+     * @param _symbol Symbol of the token
+     * @param _uri URI for token metadata/image
+     * @param _description Description of the token
+     * @param _totalSupply Total supply of the token
+     * @param _lockedSupply Amount of supply that is locked
+     * @param _unlockDate Timestamp when locked tokens become available
+     */
+    function registerToken(
+        address _tokenAddress,
+        string calldata _name,
+        string calldata _symbol,
+        string calldata _uri,
+        string calldata _description,
+        uint256 _totalSupply,
+        uint256 _lockedSupply,
+        uint256 _unlockDate
+    ) external isFactoryAutorized {
+        TokenInfo memory newToken = TokenInfo({
+            name: _name,
+            symbol: _symbol,
+            uri: _uri,
+            description: _description,
+            owner: msg.sender,
+            totalSupply: _totalSupply,
+            lockedSupply: _lockedSupply,
+            unlockDate: _unlockDate
+        });
+        s_tokens[_tokenAddress] = newToken;
+        s_tokenAddresses.push(_tokenAddress);
+    }
+
+    /**
+     * @dev Get the total number of registered tokens
+     * @return Total number of tokens in storage
+     */
+    function getTokenCount() external view returns (uint256) {
+        return s_tokenAddresses.length;
+    }
+
+    /**
+     * @dev Get latest tokens with their full information
+     * @param _count Number of recent tokens to return
+     * @return addresses Array of token addresses
+     * @return infos Array of token information
+     */
+    function getLatestTokens(uint256 _count) 
+        external 
+        view 
+        returns (address[] memory addresses, TokenInfo[] memory infos) 
+    {
+        address[] memory latestTokens = new address[](_count);
+        TokenInfo[] memory tokenInfos = new TokenInfo[](_count);
+
+        for (uint256 i = 0; i < _count; i++) {
+            latestTokens[i] = s_tokenAddresses[s_tokenAddresses.length - 1 - i];
+            tokenInfos[i] = s_tokens[latestTokens[i]];
+        }
+        return (latestTokens, tokenInfos);
+    }
+
+    /**
+     * @dev Get information about a specific token
+     * @param _tokenAddress Address of the token to query
+     * @return TokenInfo struct with token details
+     */
+    function getTokenInfo(address _tokenAddress) external view returns (TokenInfo memory) {
+        return s_tokens[_tokenAddress];
+    }
+
+    /**
+     * @dev Set the OgmaFactory address
+     * @param _ogmaFactory Address of the OgmaFactory contract
+     */
+    function setOgmaFactory(address _ogmaFactory) external onlyOwner {
+       s_ogmaFactory = _ogmaFactory;
+    }
 }
